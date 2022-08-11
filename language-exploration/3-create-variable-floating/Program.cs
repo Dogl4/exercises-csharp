3feacd66b7cc48da0aed16604f4dbc533e8a681c
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-11 12:04:44 -0300

    🚧 chore: exercice of create variables floating

---
+++ language-exploration/3-create-variable-floating/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace create_variables_floating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 - Modifed variable floating");
+
+            double salary = 3000.13;
+            Console.WriteLine(salary);
+
+            salary = salary + 1;
+            Console.WriteLine(salary);
+
+            Console.WriteLine("Press enter key to exit");
+            Console.ReadLine();
+        }
+    }
+}
