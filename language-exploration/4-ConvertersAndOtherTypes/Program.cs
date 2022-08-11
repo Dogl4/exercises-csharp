cbd64e054d6967c25a4b81478a700b7216b8f31c
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-11 18:28:14 -0300

    🚧 chore: added exercice of ConvertersAndOtherTypes

---
+++ language-exploration/4-ConvertersAndOtherTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4_ConvertersAndOtherTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 3000.185;
+            Console.WriteLine(salary);
+  
+            int salaryInt = (int)salary;
+            Console.WriteLine(salaryInt);
+
+            long x;
+            x = 20000000000;
+            Console.WriteLine(x);
+
+            short y;
+            y = 15000;
+            Console.WriteLine(y);
+
+            Console.WriteLine("Press enter key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
