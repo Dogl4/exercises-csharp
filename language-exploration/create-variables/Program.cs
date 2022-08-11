90f50bedac90d89b0817e2eb64cc5b94b69a8804
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-08-11 11:46:31 -0300

    🚧 chore: exercice of create variables
    create .gitignore, Program.cs,
    README.md and create-variables.csproj

---
+++ language-exploration/create-variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace create_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 - Modifed variable");
+
+            int age = 27;
+            Console.WriteLine("My age is: " + age);
+            age = 5 * 2 - 6;
+            Console.WriteLine("My age is: " + age);
+            age = 5 * 2 - 6;
+            Console.WriteLine("My age is: " + age);
+
+            Console.WriteLine("Input enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
